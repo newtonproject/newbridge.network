caae7319a9f4858ca27cdae3a02b21022c298480
arisac <arisac@users.noreply.github.com>
2020-12-24 08:05:28 +0000

    deploy: c782f22a87b1bf1ff261b5cbc5935aa1d82fea80

--- en.search.min.9a50cddb63d1ec192b347bdbbe5d3f2cfcc8e4d8084b43dc7704eb301aa67b7a.js
+++ en.search.min.9a50cddb63d1ec192b347bdbbe5d3f2cfcc8e4d8084b43dc7704eb301aa67b7a.js
@@ -1,9 +1,9 @@
 'use strict';(function(){const input=document.querySelector('#navbar-search-input');const results=document.querySelector('#navbar-search-results');if(!input){return}
-input.addEventListener('focus',init);input.addEventListener('focus',search);input.addEventListener('keyup',search);input.addEventListener('change',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
+input.addEventListener('focus',init);input.addEventListener('focus',search);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(event){if(input===document.activeElement){return;}
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/zh.search-data.min.3aa3f33914dcd6088eab45ea2acbef1b84bf07cd1180002c1be70e10f9c39ffe.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.744971d291f56b36ae3c0fd7833bdaa23bbc1afca48a34858318097fe52f857b.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.newdocsSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href><h4></h4><small></small></a></li>');const a=li.querySelector('a'),title=li.querySelector('h4'),small=li.querySelector('small');a.href=page.href;title.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
